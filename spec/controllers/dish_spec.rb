c3dba41302e9c827b805525f7d0408c6e2bf07f8
hoasung01 <nguyenngochai.shipagent@gmail.com>
2015-07-14 14:37:36 +0700

    added rspec for ransack

--- spec/controllers/dish_spec.rb
+++ spec/controllers/dish_spec.rb
@@ -10,8 +10,25 @@ RSpec.describe DishesController, type: :controller do
         expect(assigns(:dishes).length).to eq 10
       end
     end
-  end
 
+    context 'ransack search fuzzy by title or description' do
+
+      let!(:dish1) { FactoryGirl.create(:dish, title: 'Frozen banana', description: 'This freeze-ahead dinner party dessert') }
+      let!(:dish2) { FactoryGirl.create(:dish, title: 'Beef Wellington', description: 'a show-stopping centrepiece on a special occasion') }
+
+      context 'finds title or description' do
+        before { get :index, q: {title_or_description_cont: 'Frozen'} }
+
+        it "should find title or description" do
+          expect(assigns(:dishes).first).to eq dish1
+        end
+
+        it { should respond_with(:success) }
+        it { should render_template(:index) }
+      end
+    end
+  end
+    
   describe '#show' do
     let!(:dish) { FactoryGirl.create(:dish) }
 
