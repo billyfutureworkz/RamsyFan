fba16d7143731dae5f9030ceb9c5ad5c7870138d
hoasung01 <nguyenngochai.shipagent@gmail.com>
2015-07-15 15:07:16 +0700

    hotfix migration file

--- db/migrate/20150715042636_add_user_to_dishes.rb
+++ db/migrate/20150715042636_add_user_to_dishes.rb
@@ -1,4 +1,4 @@
-rclass AddUserToDishes < ActiveRecord::Migration
+class AddUserToDishes < ActiveRecord::Migration
   def change
     add_reference :dishes, :user, index: true, foreign_key: true
   end
